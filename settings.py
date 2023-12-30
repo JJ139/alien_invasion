e50d7e35540db0697d9b56b43b2c4eb1d52e791f
JJ139 <jehjoosten@gmail.com>
2023-12-30 19:32:14 +0100

    Ending the Game: Aliens that Reach the Bottom of the Screen

--- settings.py
+++ settings.py
@@ -22,6 +22,6 @@ class Settings:
 
         # Alien settings
         self.alien_speed = 0.1
-        self.fleet_drop_speed = 30 # 10
+        self.fleet_drop_speed = 50 # 10
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
