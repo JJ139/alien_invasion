3cc7769ad1caadb77c1cdd206d6925bdc5433529
JJ139 <jehjoosten@gmail.com>
2023-12-30 20:43:15 +0100

    Scoring: Updating the Score as Aliens Are Shot Down

--- settings.py
+++ settings.py
@@ -32,8 +32,11 @@ class Settings:
         self.bullet_speed = 3.0
         self.alien_speed = 0.5
 
-        # fleet_direction of 1 represents right; -1 represents left.
+        # Fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
+
+        # Scoring
+        self.alien_points = 50
 
     def increase_speed(self):
         """Increase speed settings."""
